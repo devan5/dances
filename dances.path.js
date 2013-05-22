039de66fca6de38f72c229569419345bbc48d3b3
devan5 <devan5.pan@gmail.com>
2013-05-22 21:17:20 +0800

    add dances.path

---
+++ dances.path.js
@@ -0,0 +1,15 @@
+window.require && require.conf("paths", {
+	// jQuery
+	jQ17       : "lib/jquery-1.7.2.min",
+	// dancesJs
+	dJs        : "dances.javascript/dances.javascript",
+	// dancesDom
+	dDom       : "dances.dom/dances.dom",
+	// dancesHttp
+	dHttp      : "dances.http/dances.http",
+	// dancesPattern
+	dPattern   : "dances.pattern/dances.pattern",
+
+	// dancesAudio
+	dancesAudio: "dances.plugins/dances.audio/dances.audio"
+});
