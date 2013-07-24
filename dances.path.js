c4147d5a67139ae5c3f459cbf5e78f5e5c2909fd
devan5 <devan5.pan@gmail.com>
2013-07-24 18:27:12 +0800

    add jquery 1.10.2 add dances.path

--- dances.path.js
+++ dances.path.js
@@ -1,35 +1,39 @@
 "function" === typeof window.define && define.amd && require.conf("paths", {
 
-	// jQuery
-	jQ17       : "lib/jquery-1.7.2.min",
+    // jQuery
+    jQ17       : "lib/jquery-1.7.2.min",
+    jQ102      : "lib/jquery-1.10.2.min",
 
-	// dancesJs
-	dJs        : "dances.javascript/dances.javascript",
-	// dancesNative
-	dNative    : "dances.javascript/dances.nativeExtend",
+    // dancesJs
+    dJs        : "dances.javascript/dances.javascript",
+    // dancesNative
+    dNative    : "dances.javascript/dances.nativeExtend",
 
-	// dancesDom
-	dDom       : "dances.dom/dances.dom",
+    // dancesDom
+    dDom       : "dances.dom/dances.dom",
 
-	// dancesHttp
-	dHttp      : "dances.http/dances.http",
+    // dancesHttp
+    dHttp      : "dances.http/dances.http",
 
-	// dancesPattern
-	dPattern   : "dances.pattern/dances.pattern",
+    // dancesPattern
+    dPattern   : "dances.pattern/dances.pattern",
 
-	// dancesKlass
-	dKlass   : "dances.pattern/dances.klass",
+    // dancesKlass
+    dKlass     : "dances.pattern/dances.klass",
 
-	// dancesObserve
-	dObserve   : "dances.pattern/dances.observe",
+    // dancesObserve
+    dObserve   : "dances.pattern/dances.observe",
 
-	// dancesInterface
-	dInterface   : "dances.pattern/dances.interface",
+    // dancesInterface
+    dInterface : "dances.pattern/dances.interface",
 
-	// dancesAudio
-	dancesAudio: "dances.plugins/dances.audio/dances.audio",
+    // dancesAudio
+    dancesAudio: "dances.plugins/dances.audio/dances.audio",
 
-	// plugins
-	// imgReady
-	imgReady: "dances.plugins/imgReady/imgReady"
+    // plugins
+    // imgReady
+    imgReady   : "dances.plugins/imgReady/imgReady",
+
+    // dancesFrogImg
+    dancesFrogImg   : "dances.plugins/dances.frogImg/dances.frogImg"
 });
